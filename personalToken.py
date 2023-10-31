4acb9729ef22cb11245b3f8e6ffbe5ea1b2d8e43
ShivamKumarTandon123 <146574429+ShivamKumarTandon123@users.noreply.github.com>
2023-10-31 13:53:33 -0400

    Create personalToken.py

---
+++ personalToken.py
@@ -0,0 +1,5 @@
+IEX_CLOUD_API_TOKEN = 'pk_699aca2c23a44a70b9260d35c8722414'
+
+# This IEX Cloud API Token is reserved to be used by Utsha K. Saha.
+# The token is a 7-day Free Trial Token used to test and debug this project.
+# In order to test / use this code, please replace the token above with a valid IEX Cloud API Token.
